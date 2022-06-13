98fcfa7b55c31587536eb750b518992368c28fe2
Biola <52200708+decorouz@users.noreply.github.com>
2022-06-13 17:51:23 +0100

    Collectstatic files

--- afex/settings/common.py
+++ afex/settings/common.py
@@ -18,9 +18,6 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
-
-
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -188,7 +185,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
